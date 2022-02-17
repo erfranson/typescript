58538dbf35f94d00fea6e97c579e16a9f587ce2d
Ethan Franson <ethan.r.franson@gmail.com>
2022-02-17 01:21:19 -0700

    unknown type

--- app.ts
+++ app.ts
@@ -1,5 +1,10 @@
 let userInput: unknown;
-
+let userName: string;
 // you can use any type 
-userInput = 5
-userInput = 'Etha'
+userInput = 5;
+userInput = 'Ethan';
+//cant set unkown to a value with a set type since it can't guarantee to be that value you have to check first 
+// use unknown over any
+if(typeof userInput === "string"){
+    userName = userInput
+}
